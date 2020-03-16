5e179a31e581896b42bf960d77e0dd98efaa0bfd
yangyanyin <yangyanyin@aliyun.com>
2020-03-16 14:10:21 +0800

    about

--- src/pages/about/App.vue
+++ src/pages/about/App.vue
@@ -1,23 +1,48 @@
 <template>
   <Layout>
-    <div class="about-bg">
-      <img class="web-img" src="../../assets/images/about/about-banner-web.jpg" />
-      <img class="wap-img" src="../../assets/images/about/about-banner-wap.jpg" />
+    <div class="about-banner"></div>
+
+    <!-- 我们是谁 -->
+    <div class="we pc-max-width clearfix margin-t-80">
+      <img class="left" src="../../assets/images/we-img.png" />
+      <div class="right des">
+        <h3>我们是谁</h3>
+        <h4>SGPEC，我们专业服务于精英。</h4>
+        <p>新加坡全球护照交流中心-SGPEC，总部位于法治严明的新加坡，是多国政府和机构投资移民项目的官方授权持牌机构。作为新加坡行业领先的专业移民机构，以高效快速，隐私保密著称业内。</p>
+      </div>
     </div>
-    <div class="about-main pc-max-width">
-      <div class="details">
-        <img class="web-img" src="../../assets/images/about/about-img1.png" />
-        <img class="wap-img" src="../../assets/images/about/about-img1-wap.png" />
-        <div class="animation-show">
-          <h3>关于我们</h3>
-          <p>新加坡全球护照交流中心，十年深耕新加坡，我们专注于为亚洲区域范围内的成功企业家提供第二居住和公民身份的服务，使个人或家庭成为全球公民。这是我们团队通过专业的一对一定制服务体验实现的，客户通过我们简化了成为全球公民的复杂性，并通过和相关机构保持长期稳定关系，可满足亚洲区域企业家对全球公民的需求。</p>
-          <p>新加坡全球护照交流中心立足狮城，放眼亚洲，专业服务亚洲企业成功精英。</p>
+
+    <!-- 专业 -->
+    <div class="profession margin-t-80">
+      <div class="pc-max-width content clearfix">
+        <div class="left des">
+          <h3>我们的专业在哪里？</h3>
+          <h4>为您提供专业的移民，护照定制化服务！</h4>
+          <p>我们专注于为全球高净值人群提供专业的新加坡移民、欧盟和英联邦护照身份的移民定制服务。我们机构汇聚行业顶尖移民律师团队和前移民官，指导协助高净值个人或者家庭成为世界公民。通过我们的专业化运作，最大程度上简化了您成为世界公民的复杂性；同时依托于我们的高端资源、与相关机构保持长期稳定的合作关系，可以高效满足高净值人群对成为世界公民的需求。</p>
+        </div>
+        <div class="img right">
+          <img src="../../assets/images/about-service.png" />
         </div>
       </div>
-      <div class="about-img animation-show">
-        <img class="web-img" src="../../assets/images/about/about-img2.jpg" />
-        <img class="wap-img" src="../../assets/images/about/about-img2.jpg" />
+    </div>
+
+    <!-- 我们的团队 -->
+    <div class="pc-max-width team  margin-t-80">
+      <div class="des">
+        <h3>我们的团队</h3>
+        <h4>给我们一次机会，还您一次满意的服务！</h4>
+        <p>我们与时俱进，与时偕行，坚持简单的事情重复做，重复的事情专业做，专业的事情极致做。<br />交流，交心，交易；诚信，专业，隐私。<br />给我们一次机会，还你一次满意的专业服务。</p>
       </div>
+      <ul class="clearfix">
+        <li><img src="../../assets/images/member1.png" alt=""></li>
+        <li><img src="../../assets/images/member2.png" alt=""></li>
+        <li><img src="../../assets/images/member3.png" alt=""></li>
+        <li><img src="../../assets/images/member4.png" alt=""></li>
+        <li><img src="../../assets/images/member5.png" alt=""></li>
+        <li><img src="../../assets/images/member6.png" alt=""></li>
+        <li><img src="../../assets/images/member7.png" alt=""></li>
+        <li><img src="../../assets/images/member8.png" alt=""></li>
+      </ul>
     </div>
   </Layout>
 </template>
@@ -39,74 +64,92 @@ export default {
 };
 </script>
 <style lang="less" scoped>
-.details {
-  position: relative;
-  div {
-    position: absolute;
-    top: 250px;
-    left: 40%;
-    right: 110px;
-    color: #bd8c67;
+.about-banner {
+  width: 100%;
+  height: 340px;
+  background: url('../../assets/images/about-banner.png') no-repeat top center;
+  background-size: 1920px;
+}
+
+.we {
+  display:flex;
+  flex-flow: row wrap;
+  align-items: center;
+  img {
+    float: left;
+    width: 50%;
+  }
+  >div {
+    width: 50%;
+    padding-left: 45px;
     h3 {
-      font-size: 36px;
-      margin-bottom: 50px;
+      font-size: 28px;
+    }
+    h4 {
+      font-size: 20px;
+      padding: 5px 0 20px;
     }
     p {
-      font-size: 18px;
-      line-height: 34px;
-      margin-bottom: 20px;
+      line-height: 26px;
+      font-size: 16px;
     }
   }
-  @media (max-width: 1200px) {
-    div {
-      top: 160px;
-      h3 {
-        margin-bottom: 30px;
-      }
-      p {
-        line-height: 30px;
-        font-size: 16px;
-      }
+}
+
+.profession {
+  background: #5e8198 url('../../assets/images/profession-bg.png') no-repeat top center;
+  background-size: 1920px;
+  .content  {
+    display:flex;
+    flex-flow: row wrap;
+    align-items: center;
+    height: 480px;
+  }
+  .left {
+    width: 50%;
+    padding-right: 60px;
+    >* {
+      color: #fff;
     }
   }
-  @media (max-width: 767px) {
-    div {
-      position: relative;
-      left: 0;
-      top: -50px;
-      h3 {
-        text-align: right;
-        font-size: 24px;
-        margin-bottom: 30px;
-        padding-right: 25px;
-      }
-      p {
-        line-height: 28px;
+  .img {
+    display: block;
+    width: 50%;
+    padding-left: 60px;
+    img {
+      display: block;
+      width: 100%;
+    }
+  }
+}
+
+.team {
+  text-align: center;
+  ul {
+    padding: 80px 0;
+    li {
+      float: left;
+      width: 12.5%;
+      padding: 0 10px;
+      img {
+        display: block;
+        width: 100%;
       }
     }
   }
 }
-.about-main {
-  margin-top: 90px;
-  .about-img {
-    margin: 80px 0;
+
+.des {
+  h3 {
+    font-size: 28px;
   }
-  img {
-    // display: block;
-    width: 100%;
+  h4 {
+    font-size: 20px;
+    padding: 5px 0 20px;
   }
-  @media (max-width: 767px) {
-    padding: 0 15px;
-    margin-top: 30px;
-    .about-img {
-      margin: 0 0 10px 0;
-    }
-  }
-}
-.about-bg {
-  display: block;
-  img {
-    width: 100%;
+  p {
+    line-height: 26px;
+    font-size: 16px;
   }
 }
 </style>
