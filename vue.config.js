d03e91338582c83732407c88a0bfe70caf8aac7a
yangyanyin <yangyanyin@aliyun.com>
2020-03-20 14:16:54 +0800

    evn

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ const path = require('path')
 const glob = require('glob')
 const fs = require('fs')
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? '//sgpec.sg/' : '/',
+	publicPath: process.env.VUE_APP_PUBLIC_PAGE,
 	outputDir: 'mucun-build/build/',
 }
