87561f3024309a232e7669d00bc65eb3787aa64d
yangyanyin <yangyanyin@aliyun.com>
2020-11-30 21:52:13 +0800

    new

--- src/views/emigrant/component/DetailsFour.vue
+++ src/views/emigrant/component/DetailsFour.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="singapore-office">
     <div class="introduction pc-max-width">
-      <h3>项目介绍</h3>
+      <h3 class="singapore-title">项目介绍</h3>
       <p>新加坡政治稳固，金融法规完善，拥有丰富的投资资源和财富管理人才，以及强有力的全球商业运作竞争力，是亚洲领先的私人银行和财富管理中心之一。新加坡政府针对家族办公室管理的指定基金项目推出了很多优惠计划，例如几乎免征收所有的投资收益所得税等等，是全世界的税务天堂。鉴于这些优势，新加坡已经逐步成为超高净值人群和家庭设立资产管理和投资基地的首选国家，尤其是在本地设立家族办公室。</p>
     </div>
 
-    <div class="advantage pc-max-width">
-      <h3>项目优势</h3>
+    <div class="advantage pc-max-width margin-t-80">
+      <h3 class="singapore-title">项目优势</h3>
       <ul class="clearfix">
         <li>
           <img src="../../../assets/images/singapore/singapore-office-icon2.png" />
@@ -45,7 +45,7 @@
 
     <div class="plan">
       <div class="bn">
-        <h3>新加坡政府指定的税收优惠计划</h3>
+        <h3 >新加坡政府指定的税收优惠计划</h3>
         <span>根据《新加坡所得税法》的第134章，新加坡以下几项税收优惠计划适用于在新加坡设立家族办公室管理的对应基金：</span>
         <p>1.在岸基金税收激励计划 13R </p>
         <p>2.增强型基金税收激励计划 13X</p>
@@ -104,21 +104,22 @@
     </div>
 
     <div class="process">
-      <h3>新加坡家族办公室申请流程</h3>
-      <ul>
-        <li>评估/签约</li>
-        <li>收集资料，预约新加坡金管局面试</li>
-        <li>注册2家新加坡公司 <br />家族办公室和基金公司</li>
-        <li>开立个人和公司的银行账户 <br />（包括零售和私人银行账户）</li>
-        <li>撰写商业计划书，准备EP申请资料和基金豁免文件</li>
+      <h3 class="singapore-title">新加坡家族办公室申请流程</h3>
+      <ul class="clearfix">
+        <li><i>第1步</i> 评估/签约</li>
+        <li><i>第2步</i>收集资料，预约新加坡金管局面试</li>
+        <li><i>第3步</i>注册2家新加坡公司 <br />家族办公室和基金公司</li>
+        <li><i>第4步</i>开立个人和公司的银行账户 <br />（包括零售和私人银行账户）</li>
+        <li><i>第5步</i>撰写商业计划书，准备EP申请资料和基金豁免文件</li>
       </ul>
-      <ul>
-        <li>获得工作准证EP卡</li>
-        <li>登录新加坡打指模</li>
-        <li>EP原则性批复</li>
-        <li>新加坡金管局面试</li>
-        <li>递交新加坡人力部EP申请</li>
+      <ul class="clearfix">
+        <li><i>第10步</i>获得工作准证EP卡</li>
+        <li><i>第9步</i>登录新加坡打指模</li>
+        <li><i>第8步</i>EP原则性批复</li>
+        <li><i>第7步</i>新加坡金管局面试</li>
+        <li><i>第6步</i>递交新加坡人力部EP申请</li>
       </ul>
+      <p>温馨提示：以上流程时间为按过往经验评估，以相关机构实际审理时间为准。</p>
     </div>
   </div>
 </template>
@@ -130,12 +131,42 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.singapore-title {
+  position: relative;
+  font-size: 28px;
+  text-align: center;
+  padding-bottom: 20px;
+  &:after {
+    content: '';
+    position: absolute;
+    left: 50%;
+    bottom: 0;
+    width: 42px;
+    height: 5px;
+    margin-left: -21px;
+    background: #447375FF;
+  }
+  @media (max-width: 1200px) {
+    font-size: 26px;
+    padding-bottom: 18px;
+  }
+  @media (max-width: 767px) {
+    font-size: 20px;
+  }
+}
 .introduction {
+  height: 393px;
+  padding-top: 60px;
   text-align: center;
   background: url('../../../assets/images/singapore/singapore-office-icon1.jpg') no-repeat top center;
-  height: 393px;
+  .singapore-title {
+    color: #fff;
+    &:after {
+      background: #fff;
+    }
+  }
   p {
-    padding: 0 160px;
+    padding: 40px 160px 0;
     line-height: 28px;
     color: #fff;
     font-size: 16px;
@@ -154,7 +185,9 @@ export default {
         margin-right: 0;
       }
       p {
-        height: 60px;
+        height: 85px;
+        margin-top: 15px;
+        font-size: 15px;
       }
       img {
         display: inline-block;
@@ -167,12 +200,15 @@ export default {
 
 .plan {
   background: #e5e2d9;
-  padding: 0 20px;
   .bn {
     height: 320px;
     text-align: center;
     color: #fff;
     background: url('../../../assets/images/singapore/singapore-office-icon10.jpg') no-repeat top center;
+    h3 {
+      padding: 90px 0 5px;
+      font-size: 24px;
+    }
     span {
       display: block;
       line-height: 24px;
@@ -184,7 +220,8 @@ export default {
     }
   }
   .table {
-    padding-top: 40px;
+    padding: 40px 20px 0;
+
     text-align: center;
     h3 {
       font-size: 24px;
@@ -233,5 +270,44 @@ export default {
 
 .process {
   background: #e5e2d9;
+  padding: 80px 20px;
+  ul {
+    padding: 50px 0;
+    li {
+      position: relative;
+      float: left;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 17%;
+      height: 120px;
+      padding: 20px 10px 0;
+      margin-right: 3.75%;
+      line-height: 22px;
+      font-size: 12px;
+      background: #fff;
+      text-align: center;
+      border-radius: 10px;
+      box-shadow: 0 0 5px 0 #b1b1b1;
+      &:last-child {
+        margin-right: 0;
+      }
+      i {
+        position: absolute;
+        left: 15px;
+        top: -10px;
+        width: 60px;
+        height: 46px;
+        line-height: 46px;
+        background: #447375;
+        color: #fff;
+      }
+    }
+  }
+  p {
+    padding-top: 10px;
+    text-align: center;
+    font-size: 12px;
+  }
 }
 </style>
