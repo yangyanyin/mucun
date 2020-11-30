2bf540bc929c18960195acbd8ad4f9ced57abbd9
yangyanyin <yangyanyin@aliyun.com>
2020-11-30 21:21:32 +0800

    s

--- src/views/emigrant/component/DetailsFour.vue
+++ src/views/emigrant/component/DetailsFour.vue
@@ -7,7 +7,7 @@
 
     <div class="advantage pc-max-width">
       <h3>项目优势</h3>
-      <ul>
+      <ul class="clearfix">
         <li>
           <img src="../../../assets/images/singapore/singapore-office-icon2.png" />
           <p>通过家族办公室可以过得EP身份，可避免金融信息交换</p>
@@ -44,54 +44,54 @@
     </div>
 
     <div class="plan">
-      <div class="">
+      <div class="bn">
         <h3>新加坡政府指定的税收优惠计划</h3>
-        <p>根据《新加坡所得税法》的第134章，新加坡以下几项税收优惠计划适用于在新加坡设立家族办公室管理的对应基金：</p>
+        <span>根据《新加坡所得税法》的第134章，新加坡以下几项税收优惠计划适用于在新加坡设立家族办公室管理的对应基金：</span>
         <p>1.在岸基金税收激励计划 13R </p>
         <p>2.增强型基金税收激励计划 13X</p>
       </div>
       <div class="table">
         <h3>重点的新加坡基金税务豁免政策</h3>
-        <ul>
+        <ul class="first clearfix">
           <li>税务优惠计划</li>
           <li>13R</li>
           <li>13X</li>
         </ul>
-        <ul>
+        <ul class="clearfix">
           <li>投资基金公司所属地</li>
           <li>新加坡的税务居民</li>
           <li>没有要求</li>
         </ul>
-        <ul>
+        <ul class="clearfix">
           <li>投资公司年度支出</li>
           <li>支出不少于20万新元</li>
           <li>支出不少于20万新元</li>
         </ul>
-        <ul>
+        <ul class="clearfix">
           <li>基金管理规模</li>
           <li>500万新币</li>
           <li>5000万新元或以上</li>
         </ul>
 
-        <ul>
+        <ul class="clearfix">
           <li>基金管理</li>
           <li>基金管理者需要常驻新加坡</li>
           <li>基金管理者需要常驻新加坡</li>
         </ul>
 
-        <ul>
+        <ul class="clearfix">
           <li>审批要求</li>
           <li>新加坡要新加坡金管局比准才可享受13R下的税务豁免计划</li>
           <li>新加坡要新加坡金管局比准才可享受13X下的税务豁免计划</li>
         </ul>
 
-        <ul>
+        <ul class="clearfix">
           <li>所得税申报</li>
           <li>必须每年向税务局申报</li>
           <li>必须每年向税务局申报</li>
         </ul>
 
-        <ul>
+        <ul class="clearfix">
           <li>向新加坡金管局提交年度声明</li>
           <li>需要</li>
           <li>需要</li>
@@ -100,26 +100,25 @@
           <br />13X计划的单一家族办公室可以获得3个EP配额
           <br />在新加坡设立家族办公室可以享受超过100个避免双重征税的扩国协定
         </p>
-
-        <div class="process">
-          <h3>新加坡家族办公室申请流程</h3>
-          <ul>
-            <li>评估/签约</li>
-            <li>收集资料，预约新加坡金管局面试</li>
-            <li>注册2家新加坡公司 <br />家族办公室和基金公司</li>
-            <li>开立个人和公司的银行账户 <br />（包括零售和私人银行账户）</li>
-            <li>撰写商业计划书，准备EP申请资料和基金豁免文件</li>
-          </ul>
-          <ul>
-            <li>获得工作准证EP卡</li>
-            <li>登录新加坡打指模</li>
-            <li>EP原则性批复</li>
-            <li>新加坡金管局面试</li>
-            <li>递交新加坡人力部EP申请</li>
-          </ul>
-        </div>
-
       </div>
+    </div>
+
+    <div class="process">
+      <h3>新加坡家族办公室申请流程</h3>
+      <ul>
+        <li>评估/签约</li>
+        <li>收集资料，预约新加坡金管局面试</li>
+        <li>注册2家新加坡公司 <br />家族办公室和基金公司</li>
+        <li>开立个人和公司的银行账户 <br />（包括零售和私人银行账户）</li>
+        <li>撰写商业计划书，准备EP申请资料和基金豁免文件</li>
+      </ul>
+      <ul>
+        <li>获得工作准证EP卡</li>
+        <li>登录新加坡打指模</li>
+        <li>EP原则性批复</li>
+        <li>新加坡金管局面试</li>
+        <li>递交新加坡人力部EP申请</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -141,5 +140,98 @@ export default {
     color: #fff;
     font-size: 16px;
   }
+}
+
+.advantage {
+  text-align: center;
+  ul {
+    padding: 0 150px;
+    li {
+      float: left;
+      width: 16%;
+      margin: 30px 12% 30px 0;
+      &:nth-child(4n+0) {
+        margin-right: 0;
+      }
+      p {
+        height: 60px;
+      }
+      img {
+        display: inline-block;
+        width: 100%;
+        max-width: 160px;
+      }
+    }
+  }
+}
+
+.plan {
+  background: #e5e2d9;
+  padding: 0 20px;
+  .bn {
+    height: 320px;
+    text-align: center;
+    color: #fff;
+    background: url('../../../assets/images/singapore/singapore-office-icon10.jpg') no-repeat top center;
+    span {
+      display: block;
+      line-height: 24px;
+      padding-bottom: 30px;
+    }
+    p {
+      font-size: 18px;
+      line-height: 26px;
+    }
+  }
+  .table {
+    padding-top: 40px;
+    text-align: center;
+    h3 {
+      font-size: 24px;
+      margin-bottom: 30px;
+    }
+    ul {
+      margin-bottom: 10px;
+      li {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        float: left;
+        width: 32.6%;
+        margin-right: 1%;
+        height: 58px;
+        background: #fff;
+        padding: 0 30px;
+        &:first-child {
+          font-size: 16px;
+        }
+        &:last-child {
+          margin-right: 0;
+        }
+      }
+      &.first {
+        height: 76px;
+        li {
+          height: 76px;
+          color: #447375;
+          font-size: 16px;
+        }
+      }
+      &:nth-child(odd) {
+        li {
+          background: #F7F5EF;
+        }
+      }
+    }
+    p {
+      margin-top: 50px;
+      font-size: 12px;
+      line-height: 24px;
+    }
+  }
+}
+
+.process {
+  background: #e5e2d9;
 }
 </style>
