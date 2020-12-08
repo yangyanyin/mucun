97757ce9e0a2b2119246dc8a53a7b2028a50ef4b
yangyanyin <yangyanyin@aliyun.com>
2020-12-08 22:01:39 +0800

    new

---
+++ src/components/EnrollmentPlan.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <input type="text" placeholder="姓名" />
+    <input type="text" placeholder="电话" />
+    <input type="text" placeholder="邮箱" />
+    <section>
+      <option value="年龄">年龄</option>
+      <option value="18岁以上">18岁以上</option>
+      <option value="18岁以下">18岁以下</option>
+    </section>
+    <section>
+      <option value="年龄">英语等级</option>
+      <option value="PETS－1">PETS－1</option>
+      <option value="PETS－2">PETS－2</option>
+      <option value="PETS－3">PETS－3</option>
+      <option value="PETS－4">PETS－4</option>
+      <option value="PETS－5">PETS－5</option>
+    </section>
+    <section>
+      <option value="年龄">最高学历</option>
+      <option value="初中">初中</option>
+      <option value="高中">高中</option>
+      <option value="大专">大专</option>
+      <option value="本科">本科</option>
+      <option value="研究生">研究生</option>
+      <option value="硕士">硕士</option>
+      <option value="博士">博士</option>
+    </section>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
