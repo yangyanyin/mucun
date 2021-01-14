dce5361dc494bb4c8431ebc443306870aa02ed0b
yangyanyin <yangyanyin@aliyun.com>
2021-01-14 11:00:56 +0800

    n

--- src/views/home/component/banner.vue
+++ src/views/home/component/banner.vue
@@ -2,12 +2,12 @@
   <div class="banner">
     <swiper :options="swiperOption">
       <swiper-slide>
-        <p class="p1">Second citizanship is not just a passport <br /> it is about family, where your family feels at home </p>
+        <p class="p1">Second CITIZENSHIP is not just a passport <br /> it is about family, where your family feels at home </p>
         <p class="p2">海外身份规划<br />这不仅仅是第二身份，更是您海外幸福生活的保障</p>
         <img @load="imgLoad" src="../../../assets/images/banner1.jpg" />
       </swiper-slide>
       <swiper-slide>
-        <p class="p1">Second citizanship is not just a passport <br /> it is about family, where your family feels at home </p>
+        <p class="p1">Second CITIZENSHIP is not just a passport <br /> it is about family, where your family feels at home </p>
         <p class="p2">海外身份规划<br />这不仅仅是第二身份，更是您海外幸福生活的保障</p>
         <img src="../../../assets/images/banner2.jpg" />
       </swiper-slide>
